749188d5abc166af837d862bf1b7aa7e2449596f
lambert <lambert.coen@yahoo.com>
2023-09-19 22:48:41 +0300

    in progress

--- hw17/books_hystrix/books-app/src/main/java/ru/otus/homework/books/BooksApplication.java
+++ hw17/books_hystrix/books-app/src/main/java/ru/otus/homework/books/BooksApplication.java
@@ -2,12 +2,14 @@ package ru.otus.homework.books;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
-public class Main {
+@EnableCircuitBreaker
+public class BooksApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class);
+        SpringApplication.run(BooksApplication.class);
 
         System.out.println("""
                 Чтобы перейти на страницу сайта открывай: http://localhost:8080
---
+++ hw17/books_hystrix/books-app/src/test/java/ru/otus/homework/books/BooksApplicationTest.java
@@ -0,0 +1,10 @@
+package ru.otus.homework.books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class BooksApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
