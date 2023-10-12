7006322112e568e5d454f0f82bb7a386ad62d3e9
lambert <lambert.coen@yahoo.com>
2023-10-13 02:35:18 +0300

    minor

--- coursework/staff/staff/src/main/java/ru/otus/coursework/staff/StaffApplication.java
+++ coursework/staff/staff/src/main/java/ru/otus/coursework/staff/StaffApplication.java
@@ -10,30 +10,11 @@ public class StaffApplication {
         SpringApplication.run(StaffApplication.class, args);
         System.out.println("""
                 
-                Включается: artifactId: spring-boot-starter-data-rest
+                UI для управления сотрудниками: http://localhost:8080
                 
-                Метаданные про REST API
-                http://localhost:8080/api/profile
-
-                Прямые обращения :
-                http://localhost:8080/api/employee?page=2&size=5
-                http://localhost:8080/api/job?page=2&size=4&sort=jobTitle
-                
-                HAL Explorer (artifactId:spring-data-rest-hal-explorer)
-                http://localhost:8080/api
+                HAL Explorer:                   http://localhost:8080/api
 
                 """);
     }
 
-    /*
-                Пользователи:
-                 - admin      - ADMIN,EDIT
-                 - user       - VIEW
-                 - accountant - VIEW,FIN
-                 - boss       - EDIT,FIN
-                 - hr         - EDIT
-                Пароль одинаковый: 'password'
-
-                http://localhost:8080/actuator
-     */
 }
